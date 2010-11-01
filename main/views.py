cbf38299da079805b06d206d6f6f7c74339012e4
Dominique <contact@quinode.fr>
2010-11-01 17:38:46 +0100

    sondages en cours

--- main/views.py
+++ main/views.py
@@ -6,5 +6,8 @@ from chantiers.models import Chantier
 
 @login_required
 def accueil(request):
-    liste_chantiers = Chantier.objects.all().order_by('position')
-    return render_to_response('index.html',{'liste':liste_chantiers},RequestContext(request))
+    liste_chantiers = Chantier.objects.all().order_by('position') 
+    for c in liste_chantiers:
+        c.nbsondages = c.sondages.count()
+        
+    return render_to_response('index.html',{'chantiers':liste_chantiers},RequestContext(request))
