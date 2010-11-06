830f265656e6ab2e6141b15023fb2272a1cd9d4e
Dominique <contact@quinode.fr>
2010-11-06 11:10:38 +0100

    autoslug populate_from

--- chantiers/models.py
+++ chantiers/models.py
@@ -9,7 +9,7 @@ class Chantier(models.Model):
     """(Chantier description)"""
     id = exfields.UUIDField(primary_key=True)
     nom = models.CharField(blank=True, max_length=120)
-    slug = exfields.AutoSlugField()
+    slug = exfields.AutoSlugField(populate_from='nom')
     description = models.TextField(blank=True)
 #   bidon = models.TextField(blank=True)
     gdocs_folder = models.CharField(blank=True, max_length=40)
--- organisme/models.py
+++ organisme/models.py
@@ -16,7 +16,7 @@ class Organisme(models.Model):
     telephone = models.CharField(max_length=10, blank=True)
     email = models.EmailField()
     url = models.URLField(blank=True)
-    slug = exfields.AutoSlugField()
+    slug = exfields.AutoSlugField(populate_from='nom')
     reseau = models.BooleanField(default=False)
     actif = models.BooleanField(default=True)
     date_creation = exfields.CreationDateTimeField()
